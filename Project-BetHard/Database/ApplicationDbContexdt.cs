33cac117f17c7b6b0ce5fc1b8b323e9991e65179
DanielFrones <daniel.frones@gmail.com>
2022-05-19 15:30:57 +0200

    ApplicationDbContext

---
+++ Project-BetHard/Database/ApplicationDbContexdt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_BetHard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_BetHard.Database
+{
+    public class ApplicationDbContext : DbContext
+     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Wallet> Wallets { get; set; }
+    }
+}
