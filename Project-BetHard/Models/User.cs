a8622943f15c8ff2aecee1da8fa6a9a9de636ba8
Gaztly <89972155+Gaztly@users.noreply.github.com>
2022-05-19 15:05:55 +0200

    Added User-class

---
+++ Project-BetHard/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_BetHard.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+        public Wallet Wallet { get; set; }
+    }
+}
