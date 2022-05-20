a8484bd2d59a40c7caca650e98d4db56c68ae6aa
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-05-20 14:52:49 +0200

    added react router, added 4 views

--- bethard-frontend/src/App.js
+++ bethard-frontend/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/views/Home";
+import Login from "./components/views/Login";
+import NotFound from "./components/views/NotFound";
+import Register from "./components/views/Register";
 
 function App() {
     return (
-        <div className="App">
-            <h1>React frontend för bethard</h1>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
     );
 }
 
---
+++ bethard-frontend/src/components/views/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Register() {
+    return <div>This is the register page</div>;
+}
+
+export default Register;
