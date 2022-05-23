fd689246278c2d22e758c25e8d2d456853be3dfa
DanielFrones <daniel.frones@gmail.com>
2022-05-23 17:07:57 +0200

    updated betcontroller postbet

---
+++ Project-BetHard/Migrations/20220523144527_WalletToDouble.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project_BetHard.Migrations
+{
+    public partial class WalletToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_WalletId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Balance",
+                table: "Wallets",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_WalletId",
+                table: "Users",
+                column: "WalletId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_WalletId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "Wallets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_WalletId",
+                table: "Users",
+                column: "WalletId",
+                unique: true,
+                filter: "[WalletId] IS NOT NULL");
+        }
+    }
+}
