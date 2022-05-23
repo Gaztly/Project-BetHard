f6fe07680599faa3d42361b8a708b07f2c87e3b8
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-05-23 16:16:12 +0200

    added usercontext

--- bethard-frontend/src/App.js
+++ bethard-frontend/src/App.js
@@ -5,18 +5,23 @@ import Login from "./components/views/Login";
 import NotFound from "./components/views/NotFound";
 import Register from "./components/views/Register";
 import Footer from "./components/footer/Footer";
+import UserContext from "./context/userContext";
 
 function App() {
     return (
-       <> <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-        <Footer/></>
+        <>
+            <UserContext.Provider value={""}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Router>
+                <Footer />
+            </UserContext.Provider>
+        </>
     );
 }
 
