06491dccf936299d4bf68d0b297f4f87f2c136d5
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-05-24 15:56:50 +0200

    home-page skeleton. needs more stuff

---
+++ bethard-frontend/src/components/home-page/homeLogo/HomeLogo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./HomeLogo.css";
+
+function HomeLogo() {
+    return (
+        <div id="home-logo-container">
+            <div id="home-logo-text">
+                BetHard
+                <i id="home-logo-icon" class="fa-solid fa-money-bill-1"></i>
+            </div>
+        </div>
+    );
+}
+
+export default HomeLogo;
--- bethard-frontend/src/components/views/Home.jsx
+++ bethard-frontend/src/components/views/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import HomeBlurb from "../home-page/homeBlurb/HomeBlurb";
+
+import HomeLogo from "../home-page/homeLogo/HomeLogo";
+import HomeRegisterButton from "../home-page/homeRegisterButton/HomeRegisterButton";
+
+import "./Home.css";
 
 function Home() {
-    return <div>This is the Homepage</div>;
+    return (
+        <div id="home-container">
+            <HomeLogo />
+            <HomeBlurb />
+            <HomeRegisterButton />
+        </div>
+    );
 }
 
 export default Home;
