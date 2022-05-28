1b12fa987d36fa7789645d1721f126ec38ecbdf2
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-05-28 15:19:12 +0200

    fixed login modal functionality

--- bethard-frontend/src/views/homeGuestView/HomeGuestView.js
+++ bethard-frontend/src/views/homeGuestView/HomeGuestView.js
@@ -3,11 +3,13 @@ import HomeLogo from "../../components/home-page/homeLogo/HomeLogo";
 import HomeBlurb from "../../components/home-page/homeBlurb/HomeBlurb";
 import HomeRegisterButton from "../../components/home-page/homeRegisterButton/HomeRegisterButton";
 import "./HomeGuestView.css";
+import LoginModal from "../../components/loginModal/LoginModal";
 
 function HomeGuestView() {
     return (
         <div id="home-guest-container">
             <div id="home-guest-background-image">
+                <LoginModal />
                 <HomeLogo />
                 <HomeBlurb />
                 <HomeRegisterButton />
