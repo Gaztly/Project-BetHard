3c4a5149c4a0e8b1d7d9e524c201f01cacb7878f
DanielFrones <daniel.frones@gmail.com>
2022-06-02 16:22:26 +0200

    Changed footer

--- bethard-frontend/src/Routes/RoutingPath.js
+++ bethard-frontend/src/Routes/RoutingPath.js
@@ -2,6 +2,6 @@ const Home = "/";
 const Register = "/register";
 const Login = "/login";
 const NotFound = "*";
-const About = "/about";
 
-export default { Home, Register, Login, NotFound, About };
+
+export default { Home, Register, Login, NotFound};
